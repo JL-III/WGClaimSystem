f1338e6dfb456cbfafa090bf0662c46d0ed6bda9
JL-III <jesselopez1162@gmail.com>
2023-01-22 00:57:56 -0600

    add detection for when player uses claim command to see if there is already a region in the chunk the player is standing in

--- src/main/java/com/jliii/wgclaimsystem/commands/PlayerCommands.java
+++ src/main/java/com/jliii/wgclaimsystem/commands/PlayerCommands.java
@@ -2,19 +2,24 @@ package com.jliii.wgclaimsystem.commands;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.math.BlockVector2;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
 import com.sun.source.util.Plugin;
-import org.bukkit.Bukkit;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.util.BlockVector;
 
 import java.util.Objects;
 import java.util.logging.Logger;
@@ -49,12 +54,11 @@ public class PlayerCommands implements CommandExecutor {
 
         LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
         com.sk89q.worldedit.world.World wgWorld = BukkitAdapter.adapt(player.getWorld());
-
+        player.sendMessage(ChatColor.YELLOW + "-----------wg-claim---------");
         player.sendMessage("You have used the claim command");
 //        player.sendMessage("FileConfiguration: " + fileConfiguration.getString("claim-world"));
 
         RegionContainer regionContainer = WorldGuard.getInstance().getPlatform().getRegionContainer();
-
 
         if (regionContainer == null) {
             logger.warning("RegionContainer is null");
@@ -91,9 +95,67 @@ public class PlayerCommands implements CommandExecutor {
             return true;
         }
 
-        for (String region : regionManager.getRegions().keySet()) {
-            player.sendMessage(region);
+        RegionQuery regionQuery = regionContainer.createQuery();
+        ApplicableRegionSet applicableRegionSet = regionQuery.getApplicableRegions(localPlayer.getLocation());
+
+        Chunk chunk = player.getLocation().getChunk();
+
+        int bx = chunk.getX() << 4;
+        int bz = chunk.getZ() << 4;
+        Location pt1 = new Location(player.getWorld(), bx, 0, bz);
+        Location pt2 = new Location(player.getWorld(), bx + 15, 256, bz + 15);
+        ProtectedCuboidRegion region = new ProtectedCuboidRegion("ThisIsAnId", BukkitAdapter.asBlockVector(pt1), BukkitAdapter.asBlockVector(pt2));
+        ApplicableRegionSet regions = regionManager.getApplicableRegions(region);
+
+        //TODO: CHeck if this region overlaps with any other regions
+
+        if (regions.size() > 0) {
+            player.sendMessage("This region overlaps with another region");
+            for (ProtectedRegion protectedRegion : regions) {
+                player.sendMessage(ChatColor.AQUA + "regionId: " + ChatColor.WHITE + protectedRegion.getId());
+                //TODO: spawn particles outlining the overlapping region that already exists
+//                for (BlockVector2 blockVector : protectedRegion.getPoints()) {
+//                    player.spawnParticle(Particle.FLAME, new Location(player.getWorld(), blockVector.getBlockX(), player.getLocation().getBlockY(), blockVector.getZ()), 1, 0, 0, 0, 0);
+//                }
+//                 for (int x = 0; x < 16; x++) {
+//                     for (int z = 0; z < 16; z++) {
+//                         player.spawnParticle(Particle.FLAME, chunk.getBlock(x, localPlayer.getLocation().getBlockY(), z).getLocation(), 1, 0, 0, 0, 0);
+//                     }
+//                 }
+            }
+            return true;
         }
+//        player.sendMessage("Chunk: " + chunk.getX() + ", " + chunk.getZ());
+//        for (int x = 0; x < 16; x++) {
+//            for (int z = 0; z < 16; z++) {
+//                logger.info("Block: " + x + ", " + z + " is in region: " + regionQuery.getApplicableRegions(BukkitAdapter.adapt(chunk.getBlock(x, 0, z).getLocation())).isOwnerOfAll(localPlayer));
+//            }
+//        }
+
+        if (applicableRegionSet.size() == 0) {
+            player.sendMessage("You are not currently in any regions");
+            return true;
+        } else {
+            player.sendMessage("You own all regions: " + applicableRegionSet.isOwnerOfAll(localPlayer));
+        }
+        for (ProtectedRegion protectedRegion : applicableRegionSet) {
+            player.sendMessage(ChatColor.AQUA + "regionId: " + ChatColor.WHITE + protectedRegion.getId());
+            player.sendMessage(protectedRegion.getOwners().contains(localPlayer.getUniqueId()) ? ChatColor.GREEN + "You own this region" : ChatColor.RED + "You do not own this region");
+        }
+
+
+//        applicableRegionSet.isOwnerOfAll(localPlayer);
+
+//        for (ProtectedRegion protectedRegion : regionManager.getRegions().values()) {
+//            player.sendMessage(protectedRegion.getId());
+//            player.sendMessage(protectedRegion.getOwners().contains(localPlayer.getUniqueId()) ? ChatColor.GREEN + "You own this region" : ChatColor.RED + "You do not own this region");
+//        }
+
+//        for (String region : regionManager.getRegions().keySet()) {
+//            player.sendMessage(region);
+//        }
+        player.sendMessage(ChatColor.YELLOW + "-----------wg-claim---------");
+
         return true;
     }
 
